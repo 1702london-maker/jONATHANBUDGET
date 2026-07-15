58481d89416de4b174c34789403903e704e11ac3
1702london-maker <1702london@gmail.com>
2026-07-15 15:00:27 +0100

    Hardcode Supabase credentials to fix env var baking issue

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const url = 'https://phgwkxhfedcyqapfhjjt.supabase.co'
+const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBoZ3dreGhmZWRjeXFhcGZoamp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxMTM4NDEsImV4cCI6MjA5OTY4OTg0MX0.K_pVYZt5Po3epRgKs2rSs02obQ_yp_krjoFC1mW4NQU'
 
 export const supabase = createClient(url, key, {
   realtime: {
