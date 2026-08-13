e052851a0f05cefc0143d41871ac4b8dd2509360
1702london-maker <1702london@gmail.com>
2026-08-13 17:39:14 +0100

    Switch to correct Supabase project (careroot org, Aug 9)

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const url = 'https://phgwkxhfedcyqapfhjjt.supabase.co'
-const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBoZ3dreGhmZWRjeXFhcGZoamp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxMTM4NDEsImV4cCI6MjA5OTY4OTg0MX0.K_pVYZt5Po3epRgKs2rSs02obQ_yp_krjoFC1mW4NQU'
+const url = 'https://czbtebcvyyrabynvrewt.supabase.co'
+const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6YnRlYmN2eXlyYWJ5bnZyZXd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyODI1NjUsImV4cCI6MjEwMTg1ODU2NX0.wBtjDdhlDGKfwZYFkkZ3BgzQvg-8kdJcKEll6w_sPQs'
 
 export const supabase = createClient(url, key, {
   realtime: {
